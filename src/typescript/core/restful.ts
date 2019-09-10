b0cd9841598af6d6e65d950c977e46f89dd42a7b
ljungmark <mattias@ljungmark.co>
2019-09-10 15:52:09 +0200

    Increment status

--- src/typescript/core/restful.ts
+++ src/typescript/core/restful.ts
@@ -21,6 +21,22 @@ class Restful implements RestfulInterface {
         this.state = state;
         document.querySelector('html').dataset.state = state;
     }
+
+    public update(): void {
+        console.log('update');
+    }
+
+    public increment(): void {
+        const previousState = this.state;
+
+        if (previousState !== State.Occupied) {
+            this.setState(State.Occupied);
+            document.querySelector('.status').textContent = 'Occupied';
+            document.querySelector('.increment').textContent = 'I\'m done, make room available for others';
+
+            const scrambleText = new ScrambleText(document.querySelector('.status')).play().start();
+        }
+    }
 }
 
 export { Restful };
--- src/typescript/master.ts
+++ src/typescript/master.ts
@@ -1,3 +1,15 @@
 import { Restful } from "./core/restful.js";
 
 const restful = new Restful();
+
+
+const incrementButton = document.querySelector('.increment');
+
+(function update() {
+    restful.update();
+    setTimeout(update, 999 - (Date.now() % 999));
+})();
+
+incrementButton.addEventListener('click', function() {
+    restful.increment();
+});
